d73c5fc5f1e89ca0e5a5bed8bd8920179fed2372
gamagegit <cgamage92@yahoo.com>
2022-12-06 08:42:34 -0800

    solved problem

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,8 +1,30 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums):
+        i = 0
+        pop_counter = 0
+        
+        while i < len(nums):
+            if nums[i] == "_":
+                i +=1   
+            elif i > 1 and (nums[i]==nums[i-1]==nums[i-2]):
+                print(nums)
+                nums.pop(i)
+                pop_counter +=1  
+                nums.append("_")
+            else:
+                i+=1
+        
+        print(nums)
+        return len(nums)-pop_counter
         
         
         
         
 obj = Solution()
 
+
+
+nums = [1,1,1,2,2,3] #5, nums = [1,1,2,2,3,_]
+nums1 = [0,0,0,0,0]
+
+print(obj.removeDuplicates(nums1))
